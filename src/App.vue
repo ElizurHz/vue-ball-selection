24eee4498b29e4e80c15de23f44ef649c77f3ed8
Haoze Xu <elizurhz@gmail.com>
2018-03-24 09:58:20 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+import numberSelection from './components/number-selection'
+
+export default {
+  name: 'App',
+  components: {
+    'number-selection': numberSelection
+  }
+}
+</script>
+
+<style>
+
+</style>
